453909ee7ad6883dd321463aec729dd32418ad6e
INTJ Mastermind <cleverusername.com>
2024-09-01 19:31:04 -0700

    Depricated range.py and classes.py

--- main.py
+++ main.py
@@ -7,11 +7,154 @@ with open('key.txt') as f:
 import requests
 import os
 from bs4 import BeautifulSoup
-from classes import *
+from math import sin, cos, sqrt, atan2, radians
 
 fse = requests.Session()
 
-def get_assignments(icao, max_jobs = 100):
+class CityPair:
+    '''
+    A City Pair object summarizes all the jobs between two cities.
+    This includes cargo, passenger (pax), and VIP jobs.
+    '''
+    def __init__(self, origin, destination):
+        self.origin = origin
+        self.destination = destination
+        self.range = find_range(self.origin, self.destination)
+
+        # Cargo jobs
+        self.cargo = 0 # total cargo in kg
+        self.cargo_jobs = 0 # number of cargo trips
+        self.cargo_value = 0 # value of cargo jobs
+
+        # Non-VIP passenger jobs.
+        self.pax = 0
+        self.pax_jobs = 0
+        self.pax_value = 0
+
+        # VIP jobs
+        self.vips = 0
+        self.vip_jobs = 0
+        self.vip_value = 0
+
+        # Totals
+        self.total_jobs = 0 # number of jobs in total
+        self.total_value = 0 # sum of all assignments going between the city pair.
+        self.dollars_per_nm = 0
+
+    def update_totals(self):
+        self.total_jobs = self.cargo_jobs + self.pax_jobs + self.vip_jobs
+        self.total_value = self.cargo_value + self.pax_value + self.vip_value
+        if self.range:
+            self.dollars_per_nm = int(self.total_value / self.range)
+    
+    def add_cargo(self, weight, pay):
+        '''
+        Adds a cargo job.
+        '''
+        self.cargo += weight
+        self.cargo_value += pay
+        self.cargo_jobs += 1
+        self.update_totals()
+
+    def add_pax(self, pax, pay):
+        '''
+        Adds a pax job.
+        '''
+        self.pax += pax
+        self.pax_value += pay
+        self.pax_jobs += 1
+        self.update_totals()
+
+    def add_vip(self, vips, pay):
+        '''
+        Adds a VIP job.
+        '''
+        self.vips += vips
+        self.vip_value += pay
+        self.vip_jobs += 1
+        self.update_totals()
+
+
+class Route():
+    """
+    A route is generated between two or more airports in succession.
+    For route optimization, we want to track:
+    - List of airports visited
+    - Does the route loop back on itself?
+    - Total value of all jobs along the route.
+    - Total length of the route.
+    - Average dollars per mile along the route.
+    """
+    def __init__(self, icao_1, icao_2, pay):
+        self.route = [icao_1, icao_2]
+        self.value = pay
+        self.length = 100
+        self.num_stops = 1
+        self.dollars_per_mile = self.pay / self.length
+
+    def _update(self):
+        # Re-calculates the number of stops and dollars per mile.
+        self.num_stops = len(self.route) - 1        
+        self.dollars_per_mile = self.pay / self.length
+
+    def add_leg(self, icao_next, pay, length = 100):
+        self.route.append(icao_next)
+        self.value += pay
+        self.length += length
+        self._update()
+
+    def contains_loop(self):
+        # Returns True if the route has a duplicate
+        return len(self.route) != len(set(self.route))
+    
+class Airport():
+    def __init__(self, icao = str, name = str, lat = float, long = float, alt = int, rwy_length = int):
+        self.icao = icao
+        self.name = name
+        self.lat = lat
+        self.long = long
+        self.alt = alt
+        self.rwy_length = rwy_length
+
+
+def load_apt(filename = 'apt.pkl'):
+    """
+    Loads and returns the apt.pkl file, which contains a dictionary with key being the ICAO code, and 
+    value containing an Airport object.
+    """
+    import pickle
+    with open('apt.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+def find_range(ICAO1, ICAO2):
+    """
+    Returns the range in nautical miles between two airports given their ICAO codes.
+    """
+
+    if ICAO1 not in apt or ICAO2 not in apt:
+        return False
+
+    # Approximate radius of earth in km
+    R = 6373.0
+    # Nautical Miles per KM
+    NM_per_KM = 0.54
+
+    lat1 = radians(apt[ICAO1].lat)
+    lon1 = radians(apt[ICAO1].long)
+    lat2 = radians(apt[ICAO2].lat)
+    lon2 = radians(apt[ICAO2].long)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return int(R * c * NM_per_KM)
+
+
+def get_assignments(icao, max_jobs = 10):
     """
     Returns FSE assignments from an airport based on icao code.
     Inputs:
@@ -91,5 +234,10 @@ def get_assignments(icao, max_jobs = 100):
     return cp
 
 
-if __name__ == '__main__':
+def main():
+    global apt
+    apt = load_apt()
     get_assignments('KVNY', 10)
+
+if __name__ == '__main__':
+    main()
